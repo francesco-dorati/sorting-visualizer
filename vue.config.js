0c89acd7d70cde8bb18de37cae3c7f4b5ef17a40
Francesco Dorati <fd.francescodorati@gmail.com>
2020-12-04 16:45:01 +0100

    💄 Add Title and Icon

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+module.exports = {
+  chainWebpack: (config) => {
+    config
+      .plugin('html')
+      .tap((args) => {
+        args[0].title = 'Sorting Visualizer';
+        return args;
+      });
+  },
+};
